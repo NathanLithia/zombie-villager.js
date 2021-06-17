ab8275e4deb70ee39db92170a2f1677018bfa858
NathanLithia <nathanlol5@live.com.au>
2021-06-18 06:39:51 +0930

    Update zombie-villager.js

--- zombie-villager.js
+++ zombie-villager.js
@@ -1,9 +1,8 @@
 onEvent('entity.spawned', event => {
 	if (event.entity.type == 'minecraft:zombie_villager'){
+		let NBT = event.entity.fullNBT;
+		NBT.PersistenceRequired = true
+		event.entity.fullNBT = NBT;
 		event.entity.setHeadArmorItem('minecraft:stone_button');
-		event.entity.setCustomName('§aZombie Villager');
-	}
-	if (event.entity.type == 'minecraft:villager'){
-		event.entity.setCustomName(null);
 	}
 })
